10d48df5d2c91f776496e0c8defd0bca0cde1a9a
Brady Gaster <bradyg@microsoft.com>
2020-07-22 20:03:46 -0700

    initial creation

---
+++ Controllers/LEDController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PiTopLightServer.Services;
+
+namespace PiTopLightServer.Controllers
+{
+    [ApiController]
+    public class LEDController : ControllerBase
+    {
+        private readonly ILogger<LEDController> _logger;
+        private readonly LEDService _ledService;
+
+        public LEDController(ILogger<LEDController> logger,
+            LEDService ledService)
+        {
+            _logger = logger;
+            _ledService = ledService;
+        }
+
+        [HttpGet]
+        [Route("green")]
+        public ActionResult Green()
+        {
+            _ledService.ToggleGreen();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("red")]
+        public ActionResult Red()
+        {
+            _ledService.ToggleRed();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("amber")]
+        public ActionResult Amber()
+        {
+            _ledService.ToggleGold();
+            return Ok();
+        }
+    }
+}
---
+++ Services/LEDService.cs
@@ -0,0 +1,62 @@
+using PiTop;
+using PiTopMakerArchitecture.Foundation;
+using PiTopMakerArchitecture.Foundation.Components;
+
+namespace PiTopLightServer.Services
+{
+    public class LEDService
+    {
+        PiTopModule _module;
+        FoundationPlate _plate;
+        private Led _green;
+        private Led _amber;
+        private Led _red;
+
+        public LEDService()
+        {
+            _module = new PiTopModule();
+
+            _plate = _module.GetOrCreatePlate<FoundationPlate>();
+
+            _green = _plate.GetOrCreateDevice<Led>(DigitalPort.D0);
+            _green.DisplayProperties.Add(new NamedCssColor("green"));
+
+            _amber = _plate.GetOrCreateDevice<Led>(DigitalPort.D1);
+            _amber.DisplayProperties.Add(new NamedCssColor("gold"));
+
+            _red = _plate.GetOrCreateDevice<Led>(DigitalPort.D2);
+            _red.DisplayProperties.Add(new NamedCssColor("red"));
+
+            _green.Off();
+            _amber.Off();
+            _red.Off();
+        }
+
+        private void Toggle(Led led)
+        {
+            if(led.IsOn)
+            {
+                led.Off();
+            }
+            else
+            {
+                led.On();
+            }
+        }
+
+        public void ToggleRed()
+        {
+            Toggle(_red);
+        }
+
+        public void ToggleGreen()
+        {
+            Toggle(_green);
+        }
+
+        public void ToggleGold()
+        {
+            Toggle(_red);
+        }
+    }
+}
